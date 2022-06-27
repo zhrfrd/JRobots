5011430befc096f0f7b3682a98b7808c304648b7
Farid Zouheir <zouheirjob@hotmail.com>
2022-06-27 20:19:36 +0100

    Created basic system to create file and read content

--- src/zhrfrd/jrobots/JRobots.java
+++ src/zhrfrd/jrobots/JRobots.java
@@ -1,10 +1,26 @@
 package zhrfrd.jrobots;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class JRobots {
-
 	public static void main (String [] args) {
-		System.out.println("Test");
-
+		try {
+			File my_file = new File("myJRobot.j");
+			Scanner fileReader = new Scanner (my_file);
+			if (my_file.createNewFile())
+				System.out.println("File created: " + my_file.getName());
+			else
+				System.out.println("File '" + my_file + "' already exists.");
+			while (fileReader.hasNextLine()) {
+		        String data = fileReader.nextLine();
+		        System.out.println(data);
+		    }
+			fileReader.close();
+	    } catch (IOException e) {
+	    	System.out.println("An error occurred.");
+	    	e.printStackTrace();
+	    }
 	}
-
 }
