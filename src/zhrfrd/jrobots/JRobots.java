03ef866adbdbe4a4645c14cc06e187778772f892
Farid Zouheir <zouheirjob@hotmail.com>
2022-07-02 22:10:53 +0100

    Cleaned code

--- src/zhrfrd/jrobots/JRobots.java
+++ src/zhrfrd/jrobots/JRobots.java
@@ -2,7 +2,6 @@ package zhrfrd.jrobots;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,42 +9,34 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 
 public class JRobots {
 	static JFrame frame;
 	static JPanel panel;
-//	static Robot label;
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
 	static Robot robot1 = new Robot();
 	static Robot robot2 = new Robot();
-	static File robotFile;
+	static File fileRobot;
 	static BufferedImage bufferedImage;
 	static ImageIcon imageIcon;
 	
 	public static void main (String[] args) throws IOException {
-		robotFile = new File("/Users/faridzouheir/eclipse-workspace/JRobots/res/robot.png");
-		bufferedImage = ImageIO.read(robotFile);
-		imageIcon = new ImageIcon(bufferedImage);
-//		label = new Robot();
-//		label.setIcon(imageIcon);
-		robot1 = new Robot();
-		robot2 = new Robot();
-		robot1.setIcon(imageIcon);
-		robot2.setIcon(imageIcon);
 		frame = new JFrame("JRobots");
 		panel = new JPanel();
+		robot1 = new Robot();
+		robot2 = new Robot();
+		fileRobot = new File("/Users/faridzouheir/eclipse-workspace/JRobots/res/robot.png");
+		bufferedImage = ImageIO.read(fileRobot);
+		imageIcon = new ImageIcon(bufferedImage);
+		robot1.setIcon(imageIcon);
+		robot2.setIcon(imageIcon);
 		
 		panel.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		panel.setBackground(Color.black);
-		panel.setLayout(new FlowLayout());   //or null
-		panel.add(robot1);
-		
+		panel.setLayout(null);   // Or new FlowLayout()??
 		panel.setFocusable(true);
-//		frame.getContentPane().add(new Robot());
 		frame.add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
@@ -53,55 +44,10 @@ public class JRobots {
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 		
-		//new GameFrame();
-		robot1.t.start();
-//		Dimension size = robot1.getPreferredSize();
-//		robot1.setBounds(90, 100, size.width, size.height);
-//		robot1.setVisible(true);
-		
-		robot2.t.start();
+		robot1.threadRobot.start();
+		robot2.threadRobot.start();
 		panel.add(robot1);
 		panel.add(robot2);
 		System.out.println(robot1.getX());
 	}
 }
-
-
-/*
-package zhrfrd.jrobots;
-
-import java.awt.Color;
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-
-public class JRobots {
-	static JFrame frame;
-	static JPanel panel;
-	static final int SCREEN_WIDTH = 600;
-	static final int SCREEN_HEIGHT = 600;
-	static Robot robot1 = new Robot();
-	static Robot robot2 = new Robot();
-	
-	public static void main (String[] args) {
-		frame = new JFrame("JRobots");
-		panel = new JPanel();
-		
-		panel.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
-		panel.setBackground(Color.black);
-		panel.setFocusable(true);
-		frame.getContentPane().add(new Robot());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.pack();
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-		
-		//new GameFrame();
-		robot1.t.start();
-		robot2.t.start();
-	}
-}
-*/
--- src/zhrfrd/jrobots/Robot.java
+++ src/zhrfrd/jrobots/Robot.java
@@ -1,67 +1,42 @@
 package zhrfrd.jrobots;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.Random;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class Robot extends JLabel implements Runnable{
-	public int life, direction, speed, posX, posY;
-	JLabel label;
-	File robotFile;
-	BufferedImage bufferedImage;
-	ImageIcon imageIcon;
-	Thread t;
-	public Random random;
+	protected static final long serialVersionUID = 1L;
+	protected int life, direction, speed, posX, posY;
+	protected Thread threadRobot;
+	private Random random;
+	private Dimension size;
 
 	// Constructor
 	Robot() {
-		
-		
-		random = new Random();
-//		this.setPreferredSize(new Dimension(300, 300));
 		this.life = 100;
-		t = new Thread(this, "Thread");
-		
+		threadRobot = new Thread(this, "Thread");
 	}
 	
+	// Methods
+	//Set robot position
 	public void setPosition() {
 		random = new Random();
 		posX = random.nextInt(500);
 		posY = random.nextInt(500);
-		Dimension size = this.getPreferredSize();
+		size = this.getPreferredSize();
 		this.setBounds(posX, posY, size.width, size.height);
 		
 		System.out.println(posX);
-//		this.setLocation(2, 8);
 	}
 	
-	// Methods
 	// Scan the battlefield and, if your robot finds another robot, return the direction
 	public int scan(int direction) {
-		if (enemyFound()) {
+		if (enemyFound())
 			return direction;
-		}
 
 		return 0;
 	}
-	
-//	public void paint(Graphics g) {
-//		posX = random.nextInt(200);
-//		posY = random.nextInt(200);
-////		System.out.println(posX + " - " + posY);
-//		System.out.println(this.getX() + " - " + this.getY());
-////		this.setLocation(posX, posY);
-////		g.setColor(Color.red);
-////		g.fillRect(posX, posY, 30, 30);
-//	}
 
 	// Shoot the enemy
 	public void shoot(int direction) {
@@ -91,99 +66,8 @@ public class Robot extends JLabel implements Runnable{
 
 	@Override
 	public void run () {
-		System.out.println(t.getName());
-//		
-//		label = new JLabel();
-//		robotFile = new	File("/Users/faridzouheir/eclipse-workspace/JRobots/res/robot.png");
-//		
-//		try {
-//			bufferedImage = ImageIO.read(robotFile);
-//		} catch (IOException e) {
-//			System.out.println("IOException");
-//			e.printStackTrace();
-//		}
-//		imageIcon = new ImageIcon(bufferedImage);
-//		setIcon(imageIcon);
-		
+		System.out.println(threadRobot.getName());
 		setPosition();
 		
 	}
 }
-
-
-
-/*
-package zhrfrd.jrobots;
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.util.Random;
-
-import javax.swing.JPanel;
-
-public class Robot extends JPanel implements Runnable{
-	public int life, direction, speed, posX, posY;
-	Thread t;
-	public Random random;
-
-	// Constructor
-	Robot() {
-		random = new Random();
-		this.setPreferredSize(new Dimension(300, 300));
-		this.life = 100;
-		t = new Thread(this, "Thread");
-	}
-
-	// Methods
-	// Scan the battlefield and, if your robot finds another robot, return the direction
-	public int scan(int direction) {
-		if (enemyFound()) {
-			return direction;
-		}
-
-		return 0;
-	}
-	
-	public void paint(Graphics g) {
-		posX = random.nextInt(200);
-		posY = random.nextInt(200);
-		System.out.println(posX + " - " + posY);
-		g.setColor(Color.red);
-		g.fillRect(posX, posY, 30, 30);
-	}
-
-	// Shoot the enemy
-	public void shoot(int direction) {
-		// Shoot bullet toward the direction
-	}
-
-	// Move the robot
-	public void move(int direction, int speed) {
-		// Move the robot around the map in the specific direction with the specific speed
-	}
-
-	// Get the x position of the robot
-	public int posX() {
-		return this.posX;
-	}
-
-	// Get the y position of the robot
-	public int posY() {
-		return this.posY;
-	}
-
-	// Check if there is an enemy along the direction your robot is pointing 
-	public boolean enemyFound() {
-		// If yes return true, else return false
-		return false;
-	}
-
-	@Override
-	public void run () {
-		
-		
-	}
-}
-
-*/
