2f759b40b29f6857bd46ed10dc3ee3157b25b2a3
Farid Zouheir <zouheirjob@hotmail.com>
2022-08-14 16:25:05 +0100

    refactor: create graphics package for sprites handling

---
+++ src/zhrfrd/graphics/Sprite.java
@@ -0,0 +1,64 @@
+package zhrfrd.graphics;
+
+public class Sprite {
+	public final int SIZE;
+	private int x, y; // Coordinates of the sprite inside the sprite sheet
+	private int height, width;
+	public int[] pixels;
+	private SpriteSheet sheet;
+	public static Sprite missile = new Sprite(16, 0, 2, SpriteSheet.missile);
+	public static Sprite particle_explosion = new Sprite (3, 0xaaaaaa);
+	
+	// Constructor
+	public Sprite(int size, int x, int y, SpriteSheet sheet) {
+		this.width = size;
+		this.height = size;
+		this.x = x * size;
+		this.y = y * size;
+		this.sheet = sheet;
+		SIZE = size;
+		pixels = new int [SIZE * SIZE];
+		load();
+	}
+	
+	public Sprite(int size, int colour) {
+		this.width = size;
+		this.height = size;
+		SIZE = size;
+		pixels = new int [SIZE * SIZE];
+		setColour(colour);
+	}
+	
+	// Methods
+	
+	/*
+	 * Add colour to each pixel
+	 */
+	public void setColour (int colour) {
+		for (int i = 0; i < width * height; i++)
+			pixels [i] = colour;
+	}
+	
+	/*
+	 * Load sprite
+	 */
+	private void load () {
+		for (int y = 0; y < SIZE; y++)
+			for (int x = 0; x < SIZE; x++)
+				pixels [x + y * SIZE] = sheet.pixels [(x + this.x) + (y + this.y) * sheet.SIZE];
+	}
+	
+	/*
+	 * Get width of the sprite
+	 */
+	public int getWidth () {
+		return width;
+	}
+	
+	/*
+	 * Get height of the sprite
+	 */
+	public int getHeight () {
+		return height;
+	}
+}
---
+++ src/zhrfrd/graphics/SpriteSheet.java
@@ -0,0 +1,36 @@
+package zhrfrd.graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpriteSheet {
+	private String path;
+	public final int SIZE, WIDTH, HEIGHT;
+	public int [] pixels;
+	public static SpriteSheet missile = new SpriteSheet("/res/sheets/missile.png", 48);
+	
+	public SpriteSheet(String path, int size) {
+		this.path = path;
+		SIZE = size;
+		WIDTH = size;
+		HEIGHT = size;
+		pixels = new int [SIZE * SIZE];
+		load();
+	}
+	
+	/*
+	 * Load sprite sheet
+	 */
+	private void load () {
+		try {
+			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));   // Load the image from path
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);   // "Translate" image to pixels
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
