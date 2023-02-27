bf30fea06e40a1ef5cb432585fe0753bbfd9df1e
Farid Zouheir <zouheirjob@hotmail.com>
2023-02-27 15:13:07 +0000

    refactor: Remove dead code

--- src/main/java/zhrfrd/jrobots/main/CollisionChecker.java
+++ src/main/java/zhrfrd/jrobots/main/CollisionChecker.java
@@ -5,6 +5,10 @@ import zhrfrd.jrobots.entities.Robot;
 public class CollisionChecker {
     Robot[] robots;
     
+    /**
+     * Constructor a collision checker.
+     * @param robots Array of robots.
+     */
     public CollisionChecker(Robot[] robots) {
 	this.robots = robots;
     }
@@ -35,27 +39,4 @@ public class CollisionChecker {
 	
 	return false;
     }
-    
-//    /**
-//     * Detect collision between entities.
-//     * @param robots Array containing all the robots in the battlefield.
-//     * @return True if there is a collision, false otherwise.
-//     */
-//    protected boolean checkCollision(Robot[] robots) {
-//	for (int i = 0; i < robots.length; i ++) {
-//	    for (int j = 0; j < robots.length; j ++) {
-//		if (robots[i] == null || robots[j] == null || robots[i] == robots[j]) {
-//		    break;
-//		}
-//		
-//		else if (robots[i].solidArea.intersects(robots[j].solidArea)) {
-//		    System.out.println("HIT!");
-//		    System.out.println(robots[i].getLife());
-//		    return true;
-//		}
-//	    }
-//	}
-//	
-//	return false;
-//    }
 }
